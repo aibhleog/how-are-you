4e2ad9d85dca3d4115512c2d624d4646b0e3f239
Taylor Hutchison <aibhleog@tamu.edu>
2021-02-08 16:46:51 -0600

    making work with .bashrc :fire:

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Hi, how-are-you?
 
-This project is a small script which, when paired with crontab, asks you each day to rank how you're feeling and logs it.  If you're not on your computer at the time that it prompts, it skips that day.
+This project is a small script which, when paired with your `.bashrc`, asks you each day (the first time you open your terminal that day) to rank how you're feeling and logs it. 
 
 Running the `how-are-you.py` script generates a file called `feelings.txt` which is used to store your feelings entries.  For now, I'm not going to upload my `feelings.txt`, for obvious reasons.
+
+
+To make this work with your `.basrhc`,  you can paste these lines at the end of your `.bashrc`:
+
+```
+# moves to dir with script, run it, then returns to home dir
+cd "/path/to/script/"
+python "how-are-you.py"
+cd
+```
+
+This prompt only happens the first time you open a terminal that day. Every other time you open your terminal that day, you won't see this prompt.
--- how-are-you.py
+++ how-are-you.py
@@ -16,42 +16,55 @@ import pandas as pd
 from datetime import datetime as dt
 import os
 
-# first, the prompt
-print('''
-===========================================
 
-How are you feeling, today?  Choose 0-4:
-
-0 : bad
-1 : not the best
-2 : neutral
-3 : I'm okay
-4 : good!
-
-===========================================
-''')
-
-# asking for response
-feel = input('Response:  ')
-try: int(feel)
-except: 
-	print('Need to input an integer from 0-4.')
-	feel = input('Response:  ' )
-	
-
-# logging answer in table
-# -----------------------
-# first checking file exists (only necessary the first time you run this
-if os.path.exists('feelings.txt') == False:
-	os.system('touch feelings.txt') # creates file
-
-df = pd.read_csv('feelings.txt',sep='\t',names=['feel','date'])
+# creating date string for today, will look like ex.: 08-Feb-2021
 date = dt.strftime(dt.now(),'%d-%b-%Y')
 
-filler_df = pd.DataFrame({'feel':[feel],'date':[date]})
-df = df.append(filler_df,ignore_index=True).copy()
+# first checking file exists (only necessary the first time you run this)
+if os.path.exists('feelings.txt') == False:
+	os.system('touch feelings.txt') # creates file
+	df = pd.read_csv('feelings.txt',sep='\t',names=['feel','date']) # specifying columns
+else: 
+	df = pd.read_csv('feelings.txt',sep='\t') # don't need to specify cols as they now exist
 
-df.to_csv('feelings.txt',sep='\t',index=False)
+# checking if this has already been run today
+# if it has, nothing happens
+if date not in df.date.values:
 
+	# the prompt
+	print('''
+===================================================
 
+    How are you feeling, today?  Choose 0-4:
+
+    0 : bad
+    1 : not the best
+    2 : neutral
+    3 : I'm okay
+    4 : good!
+
+===================================================
+	''')
+
+	# asking for response
+	feel = input('Response:  ')
+	try: int(feel)
+	except: 
+		print('Need to input an integer from 0-4.')
+		feel = input('Response:  ' )
+
+	print() # just for spacing
+		
+
+	# logging answer in table
+	# -----------------------
+
+	# appending today's answer to table
+	filler_df = pd.DataFrame({'feel':[feel],'date':[date]})
+	df = df.append(filler_df,ignore_index=True).copy()
+
+	# saving file
+	df.to_csv('feelings.txt',sep='\t',index=False)
+
+	os.system("clear") # clears terminal, to make it as if it was never there
 
